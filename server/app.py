a30fb2a4017a405a61b48ae1961286349ca5c3e9
masud5201~ <your.email@masud.abdi@student.moringaschool.com>
2024-02-03 23:57:59 -0800

    doen

--- server/app.py
+++ server/app.py
@@ -1,27 +1,46 @@
-#!/usr/bin/env python3
-
-from flask import Flask, jsonify, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-
-from models import db, Plant
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'  # Change the URI as needed
+db = SQLAlchemy(app)
 
-migrate = Migrate(app, db)
-db.init_app(app)
+# Replace this with the actual model definition in models.py
+class Plant(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
-api = Api(app)
+# Routes
 
-class Plants(Resource):
-    pass
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    plant_list = [{"id": plant.id, "name": plant.name, "image": plant.image, "price": plant.price} for plant in plants]
+    return jsonify(plant_list)
 
-class PlantByID(Resource):
-    pass
-        
+@app.route('/plants/<int:plant_id>', methods=['GET'])
+def get_plant(plant_id):
+    plant = Plant.query.get_or_404(plant_id)
+    return jsonify({"id": plant.id, "name": plant.name, "image": plant.image, "price": plant.price})
+
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+
+    new_plant = Plant(
+        name=data['name'],
+        image=data['image'],
+        price=data['price']
+    )
+
+    db.session.add(new_plant)
+    db.session.commit()
+
+    return jsonify({"id": new_plant.id, "name": new_plant.name, "image": new_plant.image, "price": new_plant.price}), 201
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    db.create_all()
+    app.run(port=5555,debug=True)
+
--- server/models.py
+++ server/models.py
@@ -1,9 +1,14 @@
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Plant(db.Model, SerializerMixin):
-    __tablename__ = 'plants'
 
+class Plant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+    def __repr__(self):
+        return f'<Plant {self.name}>'
